7f040d3ef0a16d18a9abeefdea3938f87f9b36ee
jaehyek <jaehyek@gmail.com>
2020-11-09 10:17:26 +0900

    minor 수정

--- jf.py
+++ jf.py
@@ -87,53 +87,55 @@ def findtextinfile(filename):
     linenumber = 0
     found = 0
 
-    # enc = detect_encoding(filename)
+    if( "axicdma_v4_4" in filename and "index.html" in filename ):
+        print("found")
 
-    encodings = ['utf-8', 'euc_kr', 'cp949', 'ascii', 'mbcs']
+    encodings = ['utf-8', 'utf-16', 'euc_kr', 'cp949', 'ascii', 'mbcs']
     enc_matched = False
     for enc in encodings:
-        f = open(filename, 'r', encoding=enc)
-        while(True) :
-            try:
-                textline = f.readline()
-                enc_matched = True
-            except:
-                enc_matched = False
-                continue
-            if len(textline) == 0 :
-                break
-            linenumber += 1
-            listfindstr = []
+        with open(filename, 'r', encoding=enc) as f :
+            while True:
+                try:
+                    textline = next(f)
+                    enc_matched = True
+                except StopIteration:
+                    break
+                except:
+                    enc_matched = False
+                    continue
 
-            if findtre == True :
-                listfindstr = refindtext.findall(textline, re.IGNORECASE)
-            else:
-                listfindstr.append( findtext )
+                linenumber += 1
+                listfindstr = []
 
-            for findstr in listfindstr :
-                pos = textline.lower().find(findstr.lower(), 0)
-                found = 0
-                if (pos != -1 ):
-                    found = 1
-                    # check and skip the long sentense
-                    linelength = len(textline)
-                    if linelength > 100 :
-                        endpos = pos + len(findstr) + 10
-                        if endpos > (linelength-1):
-                            endpos = linelength -1
-                        startpos = pos - 10
-                        if ( startpos < 0 ):
-                            startpos = 0
-                        textline = textline[startpos:endpos]
-                    textline = textline.strip()
-                    msg =  os.path.abspath(filename) + '.' + str(linenumber) + "::  " + textline
-                    try:
-                        print (msg)
-                    except:
-                        print(msg.encode("utf-8"))
-                if found == 1 :
-                    countTextMatched += 1
-        f.close()
+                if findtre == True :
+                    listfindstr = refindtext.findall(textline, re.IGNORECASE)
+                else:
+                    listfindstr.append( findtext )
+
+                for findstr in listfindstr :
+                    pos = textline.lower().find(findstr.lower(), 0)
+                    found = 0
+                    if (pos != -1 ):
+                        found = 1
+                        # check and skip the long sentense
+                        linelength = len(textline)
+                        if linelength > 100 :
+                            endpos = pos + len(findstr) + 10
+                            if endpos > (linelength-1):
+                                endpos = linelength -1
+                            startpos = pos - 10
+                            if ( startpos < 0 ):
+                                startpos = 0
+                            textline = textline[startpos:endpos]
+                        textline = textline.strip()
+                        msg =  os.path.abspath(filename) + '.' + str(linenumber) + "::  " + textline
+                        try:
+                            print (msg)
+                        except:
+                            print(msg.encode("utf-8"))
+                    if found == 1 :
+                        countTextMatched += 1
+
         if enc_matched == True :
             break
     if enc_matched == False:
